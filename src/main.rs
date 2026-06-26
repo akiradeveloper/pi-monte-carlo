be8374026e6498605400590daf664787f84ffa99
Akira Hayakawa <ruby.wktk@gmail.com>
2026-06-26 13:06:16 +0900

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use cubecl::prelude::*;
-use massively::{op, prelude::*, random};
+use massively::{op, prelude::*};
 
 #[derive(clap::Parser)]
 struct Args {
@@ -18,10 +18,10 @@ fn main() {
 
     // Setup
     let exec = {
-        let device = cubecl::wgpu::WgpuDevice::Cpu;
+        // let device = cubecl::wgpu::WgpuDevice::Cpu;
         // let device = cubecl::wgpu::WgpuDevice::IntegratedGpu(0);
         // let device = cubecl::wgpu::WgpuDevice::DiscreteGpu(0);
-        // let device = cubecl::wgpu::WgpuDevice::DefaultDevice;
+        let device = cubecl::wgpu::WgpuDevice::DefaultDevice;
         // let device = cubecl::hip::device::AmdDevice::new(0);
         // let device = cubecl::cuda::CudaDevice::new(0);
 
@@ -33,19 +33,12 @@ fn main() {
     let mut sum_pi = 0.;
 
     for i in 0..n {
-        // [0, max-1]
-        let xs =
-            random::uniform_dist_u32(&exec, 0, u32::max_value(), m as usize, i as u64).unwrap();
-        // [0., 1.]
-        let (xs,) = massively::transform(&exec, SoA1(xs.slice(..)), ToF32).unwrap();
-
-        let ys =
-            random::uniform_dist_u32(&exec, 0, u32::max_value(), m as usize, (i+1) as u64).unwrap();
-        let (ys,) = massively::transform(&exec, SoA1(ys.slice(..)), ToF32).unwrap();
+        let xs = massively::util::random::uniform_distribution_f32(&exec, m as usize, i as u64).unwrap();
+        let ys = massively::util::random::uniform_distribution_f32(&exec, m as usize, (i+1) as u64).unwrap();
 
         // Within the quarter circle -> 1, otherwise -> 0.
-        let (hits,) =
-            massively::transform(&exec, SoA2(xs.slice(..), ys.slice(..)), DetectHit).unwrap();
+        let mut hits = exec.filled(m as usize, 0).unwrap();
+        massively::transform(&exec, SoA2(xs.slice(..), ys.slice(..)), DetectHit, SoA1(hits.slice_mut(..))).unwrap();
 
         // Count the number of ones.
         let (n_hits,) = massively::reduce(&exec, SoA1(hits.slice(..)), (0,), CountHit).unwrap();
@@ -55,22 +48,6 @@ fn main() {
     }
 
     println!("pi={}", sum_pi / n as f64)
-}
-
-struct ToF32;
-#[cubecl::cube]
-impl<B> op::UnaryOp<B, (u32,)> for ToF32
-where
-    B: cubecl::Runtime,
-{
-    type Output = (f32,);
-
-    fn apply(x: (u32,)) -> (f32,) {
-        let (x,) = x;
-        let max = u32::max_value() - 1;
-        let out = x as f32 / max as f32;
-        (out,)
-    }
 }
 
 struct DetectHit;
